3aeeaf6c335362e1ab54e3bf6f1819cfb64a294c
alwisr <alwisr@gmail.com>
2021-12-01 18:24:56 +1100

    storefront api and cli checkin

--- ClientApp/src/app/components/products/products.component.spec.ts
+++ ClientApp/src/app/components/products/products.component.spec.ts
@@ -1,16 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ProductsComponent } from './products.component';
+import { async, ComponentFixture, TestBed,inject,tick } from '@angular/core/testing';
+import { Product } from 'src/app/models/products';
+import { ProductService } from 'src/app/services/product.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ProductsComponent } from 'src/app/components/products/products.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let mockData:Product[] = [{ name: 'product name', productId: 'product id',unitPrice: 10, sellingPrice: 120, description: 'description1', maximumQuantity: 1 },
+    { name: 'product name11', productId: 'product id11', unitPrice: 20, sellingPrice: 220, description: 'description1', maximumQuantity: 1 }];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductsComponent ]
+      declarations: [ProductsComponent],
+      providers: [ProductService],
+      imports: [RouterTestingModule.withRoutes([]), HttpClientTestingModule],
     })
-    .compileComponents();
+      .compileComponents();
+    fixture = TestBed.createComponent(ProductsComponent);
+    component = fixture.componentInstance;
+    component.products  = mockData;
+    fixture.detectChanges();
   }));
 
   beforeEach(() => {
@@ -19,7 +30,18 @@ describe('ProductsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should have a table on page', () => {
+    fixture.whenStable().then(() => {
+     let tabControl = fixture.nativeElement.querySelectorAll('table');
+     // debugger;
+      expect(tabControl.length).toEqual(1);
+    })
+
+  })
+  it('should have first header as Name', () => {
+    fixture.whenStable().then(() => {
+      let headerCell = fixture.nativeElement.querySelectorAll('th');
+     expect(headerCell[0].innerHTML).toBe('Name')
+    })
+  })
 });
--- Models/Product.cs
+++ Models/Product.cs
@@ -11,7 +11,6 @@ namespace storefront.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public double UnitPrice { get; set; }
-
         public double SellingPrice => (double)(UnitPrice * 1.2);
         public int? MaximumQuantity { get; set; }
     }
